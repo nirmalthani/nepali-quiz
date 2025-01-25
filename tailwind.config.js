fa6f58576c4354479b29c8f288c12c81f3e2d204
nirmalthani <89489984+nirmalthani@users.noreply.github.com>
2025-01-25 20:30:24 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+### tailwind.config.js
+```js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+```
+
+---
